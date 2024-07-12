4de68ae3fc69f500300beee81bfd5c9b945a58a2
parkjaewoo <parkzaewoo@gmail.com>
2024-07-12 19:21:47 +0900

    bash_template dag 업로드

---
+++ dags/dags_bash_with_template.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    bash_t1=BashOperator(
+        task_id='bash_t1',
+        bash_command='echo "data_interval_end: {{ data_interval_end }} "'
+    )
+
+    bash_t2=BashOperator(
+        task_id='bash_t2',
+        env={
+            'START_DATE':'{{ data_interval_start | ds }}',
+            'END_DATE':'{{ data_interval_end | da }}'
+        },
+        bash_command='echo $START_DATE && $END_DATE'
+    )
+
+    bash_t1 >> bash_t2
