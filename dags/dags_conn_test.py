acf46318989ba0e9d8cf13430f4e7f998f07389a
parkjaewoo <parkzaewoo@gmail.com>
2024-07-12 13:09:29 +0900

    커넥션 테스트용 dag 업로드

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="dags_cron_test",
+    dag_id="dags_conn_test",
     schedule=None,
     start_date=pendulum.datetime(2024, 7, 1, tz="Asia/Seoul"),
     catchup=False
