bb5e12675c237087139348ff249ab5ce22696997
parkjaewoo <parkzaewoo@gmail.com>
2024-07-12 16:54:12 +0900

    공통 외부함수 추가

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_func",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False
