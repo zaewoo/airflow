09ad108842dd14335a73181d7ed66b3077444c7a
parkjaewoo <parkzaewoo@gmail.com>
2024-07-13 15:46:07 +0900

    bash with macros dag 업로드

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -11,7 +11,7 @@ with DAG(
     # START_DATE: 전월 말일, END_DATE: 1일 전
     bash_task_1=BashOperator(
         task_id='bash_task_1',
-        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul) | ds }}',
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
              'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
