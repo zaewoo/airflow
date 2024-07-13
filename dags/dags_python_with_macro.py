94442f76de909510060cde5b7e9189fbf73a22b4
parkjaewoo <parkzaewoo@gmail.com>
2024-07-13 16:32:32 +0900

    python with macro dag 업로드

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -1,17 +1,16 @@
 from airflow import DAG
 import pendulum
-from airflow.operators.bash import BashOperator
 from airflow.decorators import task
 
 with DAG(
     dag_id='dags_python_with_macro',
     schedule='10 0 * * *',
-    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024, 7, 1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
     @task(task_id='task_using_macros',
-          templates_dict={'start_date':'{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta(months=-1, day=1)) | ds }}',
+          templates_dict={'start_date':'{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
                           'end_date':'{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
                           }
     )
@@ -26,8 +25,8 @@ with DAG(
     @task(task_id='task_direct_calc')
     def get_datetime_calc(**kwargs):
         from dateutil.relativedelta import relativedelta
+        
         data_interval_end = kwargs['data_interval_end']
-    
         prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1, day=1)
         prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
         print(prev_month_day_first.strftime('%Y-%m-%d'))
