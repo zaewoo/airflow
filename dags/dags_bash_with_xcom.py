f93e6152b3c320c20386842950792a7c0742247f
parkjaewoo <parkzaewoo@gmail.com>
2024-07-13 17:54:02 +0900

    bash with xcom dag 업로드

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -22,7 +22,7 @@ with DAG(
         task_id='bash_pull',
         env={'PUSHED_VALUE':"{{ ti.xcom_pull(key=bash_pushed') }}",
              'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
-        bash_command="echo $PUSHED_VALUE && echo $RETURN VALUE",
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE",
         do_xcom_push=False
     )
 
