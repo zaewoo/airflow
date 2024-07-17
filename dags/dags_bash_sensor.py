cb1291160fc75e85018fd2528d8f9238802d37bd
parkjaewoo <parkzaewoo@gmail.com>
2024-07-17 17:12:05 +0900

    bash sensor dag 업로드

--- dags/dags_bash_sensor.py
+++ dags/dags_bash_sensor.py
@@ -12,7 +12,7 @@ with DAG(
 
     sensor_task_by_poke = BashSensor(
         task_id='sensor_task_by_poke',
-        env={'FILE':'/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
+        env={'FILE':'/opt/airflow/files/tvCorona19VaccinestatNew/20240716/tvCorona19VaccinestatNew.csv'},
         bash_command=f'''echo $FILE && 
                         if [ -f $FILE ]; then 
                               exit 0
@@ -27,7 +27,7 @@ with DAG(
 
     sensor_task_by_reschedule = BashSensor(
         task_id='sensor_task_by_reschedule',
-        env={'FILE':'/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
+        env={'FILE':'/opt/airflow/files/tvCorona19VaccinestatNew/20240716/tvCorona19VaccinestatNew.csv'},
         bash_command=f'''echo $FILE && 
                         if [ -f $FILE ]; then 
                               exit 0
@@ -42,7 +42,7 @@ with DAG(
 
     bash_task = BashOperator(
         task_id='bash_task',
-        env={'FILE': '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
+        env={'FILE': '/opt/airflow/files/tvCorona19VaccinestatNew/20240716/tvCorona19VaccinestatNew.csv'},
         bash_command='echo "건수: `cat $FILE | wc -l`"',
     )
 
