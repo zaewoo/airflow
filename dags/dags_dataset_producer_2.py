2263d9b684e8b4821ef15b3e0e0e283cd4285b4d
parkjaewoo <parkzaewoo@gmail.com>
2024-07-18 14:15:35 +0900

    dataset consumer/producer dag 업로드

---
+++ dags/dags_dataset_producer_2.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow import Dataset
+from airflow.operators.bash import BashOperator
+import pendulum
+
+dataset_dags_dataset_producer_2 = Dataset('dags_dataset_produce_2')
+
+with DAG(
+    dag_id='dags_dataset_producer_2',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2024,4,1,tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    bash_task = BashOperator(
+        task_id='bash_task',
+        outlets=[dataset_dags_dataset_producer_2],
+        bash_command='echo "producer_2 수행 완료"'
+    )
