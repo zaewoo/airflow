60c04090ec27b0beb8875726f5a1b05f46b93269
parkjaewoo <parkzaewoo@gmail.com>
2024-07-18 10:37:57 +0900

    external task sensor dag 업로드

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.python import BranchPythonOperator
 with DAG(
     dag_id='dags_branch_python_operator',
     start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
-    schedule=None,
+    schedule='0 1 * * *',
     catchup=False
 ) as dag:
     def select_random():
